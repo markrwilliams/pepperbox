d6e3a7b1fdfddc5b8bb990f19f4c013879972291
Mark Williams <markrwilliams@gmail.com>
2015-03-21 23:23:22 -0700

    setup.py knows the difference between 2 and 3

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 if __name__ == '__main__':
     import os
+    import sys
     from setuptools import setup, find_packages
     from pepperbox._ffi import ffi
 
@@ -7,10 +8,13 @@ if __name__ == '__main__':
     if os.environ.get('INSTALL_CUSTOMIZE'):
         kwargs['pymodules'] = ['sitecustomize']
 
+    with open('requirements{}.txt'.format(sys.version_info.major)) as f:
+        requirements = list(f)
+
     setup(name='pepperbox',
           version='0.0.1',
           zip_safe=False,
-          install_requires=['spyce'],
+          install_requires=requirements,
           packages=find_packages(),
           ext_package='pepperbox',
           ext_modules=[ffi.verifier.get_extension()],
