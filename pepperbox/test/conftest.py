3f1e66a95a10d844cab21cf31187d3641a05bec8
Mark Williams <markrwilliams@gmail.com>
2015-04-21 18:34:13 -0700

    move all the loader tests into one file

--- pepperbox/test/conftest.py
+++ pepperbox/test/conftest.py
@@ -1,12 +1,3 @@
-import py.path
-import pytest
-from ._test_loaders_support import gen_category_fixture_loaders
-
-
-def pytest_namespace():
-    return {'pepperbox_loader_table': []}
-
-
 def pytest_addoption(parser):
     parser.addoption("--fixture-dir", action="store", default=None,
                      help='Directory in which the tests will write out '
@@ -14,10 +5,3 @@ def pytest_addoption(parser):
     parser.addoption('--lineage', default='a.b.c.d',
                      help='import path representing the deepest package'
                      ' to create.')
-
-
-def pytest_configure(config):
-    fixture_dir = py.path.local(config.getoption('fixture_dir'))
-    lineage = config.getoption('lineage')
-    loader_table = gen_category_fixture_loaders(fixture_dir, lineage)
-    pytest.pepperbox_loader_table = loader_table
